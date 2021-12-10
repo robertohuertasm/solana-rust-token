d5853262e914d50ad6986549255aa8cf5b10cba8
robertohuertasm <roberto.huertas@outlook.com>
2021-12-10 23:15:12 +0100

    remove tests comments

--- tests/rust-token.js
+++ tests/rust-token.js
@@ -1,18 +1,3 @@
-// const anchor = require('@project-serum/anchor');
-
-// describe('rust-token', () => {
-
-//   // Configure the client to use the local cluster.
-//   anchor.setProvider(anchor.Provider.env());
-
-//   it('Is initialized!', async () => {
-//     // Add your test here.
-//     const program = anchor.workspace.RustToken;
-//     const tx = await program.rpc.initialize();
-//     console.log("Your transaction signature", tx);
-//   });
-// });
-
 const anchor = require('@project-serum/anchor');
 const assert = require('assert');
 
